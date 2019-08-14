56b87c6dfc4d9fa71a8569ee3e8bc13c4860ba23
zxl976557204 <976557204@qq.com>
2019-08-14 22:52:01 +0800

    后端登录功能

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -62,6 +62,7 @@ AppAsset::register($this);
 
     <div class="container">
         <?= Breadcrumbs::widget([
+            'homeLink' => ['label' => '首页', 'url' => ['site/index']],
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
